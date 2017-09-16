831f839e3d08d8e4ac2268de3058382ce37c8605
Tana - HP <Tana - HP@Tana-HP>
2017-09-16 10:48:03 +0100

    version 1.1

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,16 @@
+package kata1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Tana - HP
+ */
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person person = new Person("Blas", new Date(49,8,24));
+        System.out.println(person.getName() + " tiene " + person.getAge());
+    }
+    
+}
